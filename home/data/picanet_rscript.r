44b5d6604fcfad7f35be1c8ee6e4c0b4a78b0ab8
maya70 <maya70@vt.edu>
2019-06-21 09:37:51 +0100

    updated picanet R script

--- home/data/picanet_rscript.r
+++ home/data/picanet_rscript.r
@@ -1,43 +1,48 @@
  library(readr)
  library(lubridate)
+ library(parsedate)
+
  
  # Change the unit ID to match your unit
  unitID <- "194281" 
+ source_file_path <- "C:/Users/Mai/Dropbox/Leeds/Qualdash related/Data/"
+ dest_file_path <- "C:/Bitnami/wampstack-7.0.12-0/apache2/htdocs/Qualdashv1/home/data/picanet_admission/"
+ dest_activity_path <- "C:/Bitnami/wampstack-7.0.12-0/apache2/htdocs/Qualdashv1/home/data/picanet_activity/"
+ audit_filename <- "admission.csv"
+ 
+ source = paste(source_file_path, audit_filename, sep='')
 
+ 
  # read raw admission data from its current location
- admission <- read_csv("C:/Users/Mai/Dropbox/Leeds/Qualdash related/Data/admission.csv")
+ admission <- read_csv(source)
  admission <- subset(admission, siteidscr == unitID)
  # break it into separate files for individual years
  # and store the new files in the picanet folder under documnt root 
  for(year in unique(admission$adyear)){
      tmp = subset(admission, adyear == year)     
-     fn = paste('C:/Bitnami/wampstack-7.0.12-0/apache2/htdocs/Qualdashv1/home/data/picanet_admission/', gsub(' ','', year), '.csv', sep='' )
+     fn = paste(dest_file_path, gsub(' ','', year), '.csv', sep='' )
      write.csv(tmp, fn, row.names = FALSE)
  }
 
-yfn = paste('C:/Bitnami/wampstack-7.0.12-0/apache2/htdocs/Qualdashv1/home/data/picanet_admission/avail_years.csv', sep='' )
+yfn = paste(dest_file_path ,'avail_years.csv', sep='' )
  write.csv(unique(df$adyear), yfn, row.names = FALSE)
 
 
  # read raw activity data
-  activity <- read_csv("C:/Users/Mai/Dropbox/Leeds/Qualdash related/Data/activity.csv")
+ source = paste(source_file_path, "activity.csv", sep='')
+  activity <- read_csv(source)
 
   # split activity file by year of admission
   myvars <- c("adyear", "eventidscr")
   newdata <- admission[myvars]
   df <- merge(newdata, activity, by='eventidscr')
-   #for(year in unique(df$adyear)){
-    # tmp = subset(df, adyear == year)
-    # fn = paste('C:/Bitnami/wampstack-7.0.12-0/apache2/htdocs/Qualdashv1/home/data/picanet_activity/', gsub(' ','', year), '.csv', sep='' )
-    # write.csv(tmp, fn, row.names = FALSE)
-    #}
-
+  
  # get the latest year in your data
  #maxyear <- max(admission$adyear, na.rm = TRUE)
 
 for(year in unique(df$adyear)){
    # read the admission records for the current year alone
-   fn = paste('C:/Bitnami/wampstack-7.0.12-0/apache2/htdocs/Qualdashv1/home/data/picanet_admission/', gsub(' ','', year), '.csv', sep='' )
+   fn = paste(source_file_path, 'activity/', gsub(' ','', year), '.csv', sep='' )
    cur_adm <- read.csv(fn)
    # merge the activity file
    M <- merge(cur_adm, activity, by=c('eventidscr'), all.x=T)
@@ -46,7 +51,7 @@ for(year in unique(df$adyear)){
     d = data.frame(M$eventidscr, M$addate, M$hrggroup, M$unplannedextubation, M$invventet, M$invventtt, M$intubation, M$ventilationstatus, M$avsjet, M$avsosc, M$siteidscr)
     colnames(d) <- c('eventidscr', 'addate', 'hrggroup', 'unplannedextubation', 'invventet', 'invventtt', 'intubation', 'ventiliationstatus', 'avsjet', 'avsosc', 'siteidscr')
 
-    fn = paste('C:/Bitnami/wampstack-7.0.12-0/apache2/htdocs/Qualdashv1/home/data/picanet_activity/shortactiv', gsub(' ','', year), '.csv', sep='' )
+    fn = paste(dest_activity_path, 'shortactiv' , gsub(' ','', year), '.csv', sep='' )
     write.csv(d, fn)
 }
 
